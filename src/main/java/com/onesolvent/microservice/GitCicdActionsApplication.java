36174e54e94a093f747cacb96c2168b8e2638bda
rkarambali <rkarambali@avaya.com>
2023-01-28 16:02:10 +0530

    okok

--- src/main/java/com/onesolvent/microservice/GitCicdActionsApplication.java
+++ src/main/java/com/onesolvent/microservice/GitCicdActionsApplication.java
@@ -2,10 +2,15 @@ package com.onesolvent.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GitCicdActionsApplication {
 
+	@GetMapping("/get")
+	public String get() {
+		return "CI-CD by GITHUB";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GitCicdActionsApplication.class, args);
 	}
