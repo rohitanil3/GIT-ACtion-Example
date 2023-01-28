bd6ff8e0458547cd7b19a0a5534666095428f774
rkarambali <rkarambali@avaya.com>
2023-01-28 13:53:39 +0530

    ok

--- src/main/java/com/onesolvent/microservice/GitCicdActionsApplication.java
+++ src/main/java/com/onesolvent/microservice/GitCicdActionsApplication.java
@@ -10,4 +10,12 @@ public class GitCicdActionsApplication {
 		SpringApplication.run(GitCicdActionsApplication.class, args);
 	}
 
+	
+//	echo "# GIT-ACtion-Example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/rohitanil3/GIT-ACtion-Example.git
+//	git push -u origin main
 }
